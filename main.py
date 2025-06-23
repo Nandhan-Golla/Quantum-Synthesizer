fe6dacec85bb5145cae6aed14c2db2c42f239da2
Nandhan Golla <nandhan.golla.work@gmail.com>
2025-06-23 21:16:36 +0530

    framework update

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from quantum-gan!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ qsyn/synthesis.py
@@ -0,0 +1,4 @@
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.impute import SimpleImputer
+import keras as ke 
+import logging 
